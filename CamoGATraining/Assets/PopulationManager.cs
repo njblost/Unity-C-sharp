96d4a8a034759c493d0070e692251114c2c259d4
njbeepbeep <njbeepbeep@icloud.com>
2024-02-05 12:06:54 +1100

    2 VR Basics update

---
+++ CamoGATraining/Assets/PopulationManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulationManager : MonoBehaviour
+{
+
+    // Link to the person prefab
+    public GameObject personPrefab;
+    public int populationSize = 10;
+
+    // Keep track of the people we create using a list
+    List<GameObject>
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
