50781525d16b915b5be1da5f56ddf11463e505c9
njbeepbeep <njbeepbeep@icloud.com>
2024-03-01 17:57:08 +1100

    Adjusted the naming - need to check the DNA, Brain and PopMan

---
+++ ML-Unity/Lesson 6c - Movement With Senses - timeWalking/Assets/Brain.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Brain : MonoBehaviour
+{
+    int DNALength = 2;
+    public float timeAlive;
+    public float timeWalking;
+    public DNA dna;
+    public GameObject eyes;
+
+    bool alive = true;
+    bool seeGround = true;
+
+    void OnCollisionEnter(Collision obj)
+    {
+        if (obj.gameObject.tag == "dead")
+        {
+            alive = false;
+        }
+    }
+
+    // Initialise DNA
+    public void Init()
+    {
+        // 0 Forward
+        // 1 Left
+        // 2 Right
+        dna = new DNA(DNALength, 3);
+        timeAlive = 0;
+        alive = true;
+    }
+
+    private void Update()
+    {
+        if (!alive) return;
+
+        Debug.DrawRay(eyes.transform.position, eyes.transform.forward * 10, Color.red, 10);
+        seeGround = false;
+        RaycastHit hit;
+        if (Physics.Raycast(eyes.transform.position, eyes.transform.forward * 10, out hit))
+        {
+            if (hit.collider.gameObject.tag == "platform")
+            {
+                seeGround = true;
+            }
+        }
+        timeAlive = PopulationManager.elapsed;
+
+        // Read DNA
+        float turn = 0;
+        float move = 0;
+
+        if (seeGround)
+        {
+            if (dna.GetGene(0) == 0) {move = 1; timeWalking += 1;}
+            else if (dna.GetGene(0) == 1) turn = -90;
+            else if (dna.GetGene(0) == 2) turn = 90;
+        }
+        else
+        {
+            if (dna.GetGene(1) == 0) { move = 1; timeWalking += 1;}
+            else if (dna.GetGene(1) == 1) turn = -90;
+            else if (dna.GetGene(1) == 2) turn = 90;
+        }
+
+        this.transform.Translate(0, 0, move * 0.1f);
+        this.transform.Rotate(0, turn, 0);
+    }
+}
