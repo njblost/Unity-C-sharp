aac572a08114570383c78fed849a427ef9ab5e8d
njbeepbeep <njbeepbeep@icloud.com>
2024-05-31 16:38:09 +1000

    1VR & 2-3VR Basics updates

---
+++ OLD/3VRGameBasics - Ray Interactions/Assets/Scripts/AnimateHandController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+public class AnimateHandController : MonoBehaviour
+{
+    public InputActionReference gripInputActionReference;
+    public InputActionReference triggerInputActionReference;
+
+    private Animator _handAnimator;
+    private float _gripValue;
+    private float _triggerValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _handAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimateGrip();
+        AnimateTrigger();
+    }
+
+    private void AnimateGrip()
+    {
+        _gripValue = gripInputActionReference.action.ReadValue<float>();
+        _handAnimator.SetFloat("Grip", _gripValue);
+    }
+
+    private void AnimateTrigger()
+    {
+        _triggerValue = triggerInputActionReference.action.ReadValue<float>();
+        _handAnimator.SetFloat("Trigger", _triggerValue);
+    }
+}
