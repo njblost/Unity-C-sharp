ddf9ab596992e9112f809b0c239d5904e3432a90
njblost <cut.starry.8q@icloud.com>
2026-03-14 13:08:02 +1100

    Heart animation

--- Duck Game Experimental/Assets/Scripts/MonoBehaviours/Character.cs
+++ Duck Game Experimental/Assets/Scripts/MonoBehaviours/Character.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
 
-public class Character : MonoBehaviour
+public abstract class Character : MonoBehaviour
 {
+
+    public int hitPoints;
+    public int maxHitPoints;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
